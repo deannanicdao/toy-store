a0007cfd165aea4175b646d55041f8f7b9475831
deannanicdao <deanna.nicdao@gmail.com>
2020-11-03 15:16:22 +1000

    Added seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user_1 = User.create! email: 'a@b.com', password: '123456', password_confirmation: 'password'
+
